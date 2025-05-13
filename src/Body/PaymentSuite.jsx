0cd6474792718d57b79c22c10a82ba8a7d4ed267
sameer Hussain <samsaifi076@gmail.com>
2025-05-13 23:47:51 +0530

    Payment Suite section updated

--- src/Body/PaymentSuite.jsx
+++ src/Body/PaymentSuite.jsx
@@ -1,8 +1,9 @@
 export default function PaymentSuite(){
     return (
         <div className="w-full">
-           <div className="flex justify-center pt-8">
+           <div className="flex justify-center  items-center flex-col pt-8 space-y-1">
             <h1 >Accept Payment with Razorpay Payment Suite</h1>
+            <div className="w-5 h-1 bg-green-300"></div>
             </div> 
 <div className="flex items-center justify-center ">
             <section className="w-5/12" >
